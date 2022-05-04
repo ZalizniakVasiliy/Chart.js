16833e03c692d3c75e4c41454ba44c9ae85b1a56
Vasilii Zalizniak <vzalizniak31@gmail.com>
2022-05-04 20:04:39 +0300

    add config, setup of Chart.js, output user names to chart

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,52 @@
 import users from "./users_with_rating.js";
 
 console.log(users);
+
+const labels = users;
+const data = {
+    labels: users.map(item => {
+        const getFullName = () => {
+            return `${item.firstName} ${item.lastName}`
+        }
+        return getFullName();
+    }),
+    datasets: [
+        {
+            label: 'Success indicator',
+            data: users,
+            borderColor: 'red',
+            backgroundColor: 'hsl(0, 100%, 50%, 0.5)',
+            borderWidth: 2,
+            borderRadius: Number.MAX_VALUE,
+            borderSkipped: false,
+        },
+        {
+            label: 'Current rating',
+            data: users,
+            borderColor: 'blue',
+            backgroundColor: 'hsl(250, 100%, 50%, 0.5)',
+            borderWidth: 2,
+            borderRadius: 5,
+            borderSkipped: false,
+        }
+    ]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Random user rating'
+            }
+        }
+    },
+};
+
+const myChart = new Chart(document.getElementById('myChart'), config);
